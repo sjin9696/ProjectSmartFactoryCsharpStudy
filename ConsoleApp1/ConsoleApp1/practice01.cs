8b95d961dace095d68453a24c42465933a0481b3
sjin9696 <sjin9696@gmail.com>
2020-08-06 14:17:18 +0900

    개미수열 구현 강사님께서 작성하신 코드

--- ConsoleApp1/ConsoleApp1/practice01.cs
+++ ConsoleApp1/ConsoleApp1/practice01.cs
@@ -21,7 +21,9 @@ namespace ConsoleApp1
             //input_StringFirstAlphabetPrint();   //입력받은 문자열의 글자가 최초로 등장하는 위치를 표시
             //starPiramid_print();    //삼각형 모양 피라미드를 만드시오
             //input_numMaxMinPrint(); //5개의 숫자를 입력받은 뒤 가장 작은 수와 가장 큰 수를 표시
-            sequencePrint(); //개미수열의 20번째 표시
+            sequencePrint(); //개미수열 표시
+            //aaa();
+            //bbb();
             return;
         }
         static void chineseZodiac()
@@ -30,7 +32,7 @@ namespace ConsoleApp1
 
             Console.WriteLine("태어난 년도를 입력해주세요.");
             int year_birth = int.Parse(Console.ReadLine());
-           
+
             Console.WriteLine("입력한 태어난 년도는 " + year_birth + "입니다");
             Console.WriteLine("입력한 태어난 년도의 띠는 \"" + chineseZodiacArray[(year_birth % 12)] + "\"입니다");
             Console.ReadLine();
@@ -39,13 +41,13 @@ namespace ConsoleApp1
         static void monthToSeasonPrint()
         {
             //스위치문 안쓰고 출력 방법 찾아 보기.
-            List<string> seasonArray = new List<string>() { "봄", "여름", "가을", "겨울"};
+            List<string> seasonArray = new List<string>() { "봄", "여름", "가을", "겨울" };
             int index = 0;
             Console.WriteLine("계절을 확인하고 싶은 월을 입력해주세요.(1~12)");
             int month = int.Parse(Console.ReadLine());
             if ((1 <= month) && (month <= 12))
             {
-                switch(month)
+                switch (month)
                 {
                     case 1:
                     case 2:
@@ -143,11 +145,12 @@ namespace ConsoleApp1
         {
             //1~100까지 3의 배수를 제외한 합을 구하는 함수를 작성하세요.
             int sum = 0;
-            for(int i=1;i<=100;i++)
+            for (int i = 1; i <= 100; i++)
             {
                 sum = (i % 3 == 0) ? (sum += 0) : (sum += i);
             }
             Console.WriteLine("1~100까지 3의 배수를 제외한 합은 \"" + sum + "\" 입니다.");
+            //Console.WriteLine($"1~100까지 3의 배수를 제외한 합은 \" {sum} \" 입니다.");
             Console.ReadLine();
             return;
         }
@@ -157,9 +160,9 @@ namespace ConsoleApp1
             //변환하는 프로그램을 작성하세요
             Console.WriteLine("분:초로 보고 싶은 전체 초 값을 입력하세요.(양의정수)");
             int inputTotalSecond = int.Parse(Console.ReadLine());
-            
+
             Console.WriteLine("입력한 전체 초는 \"" + inputTotalSecond + "\"초 입니다.");
-            Console.WriteLine("처리한 n분:n초는 \"" + inputTotalSecond / 60 + "분:"+
+            Console.WriteLine("처리한 n분:n초는 \"" + inputTotalSecond / 60 + "분:" +
                 inputTotalSecond % 60 + "초\" 입니다.");
             Console.ReadLine();
             return;
@@ -186,9 +189,9 @@ namespace ConsoleApp1
                         count++;
                 }
                 //문자열 체크 중복검사 처리
-                if (!tempStringList.Contains(item + ":" + count + "회"))
+                if (!tempStringList.Contains("\'" + item + "\'는\"" + count + "\"글자 입니다"))
                 {
-                    tempStringList.Add(item + ":" + count + "회");
+                    tempStringList.Add("\'" + item + "\'는\"" + count + "\"글자 입니다");
                 }
             }
             //tempStringList 출력
@@ -209,14 +212,14 @@ namespace ConsoleApp1
             //최초등장위치 처리
             foreach (char item in inputString)
             {
-                string sForm = item + "는 "+(inputString.IndexOf(item) + 1) + "번째에서 등장합니다.";
+                string sForm = item + "는 " + (inputString.IndexOf(item) + 1) + "번째에서 등장합니다.";
                 if (!tempStringList.Contains(sForm))
                     tempStringList.Add(sForm);
             }
             //tempStringList 출력
             foreach (string item in tempStringList)
                 Console.WriteLine(item);
-            
+
             Console.ReadLine();
             return;
         }
@@ -227,11 +230,11 @@ namespace ConsoleApp1
             //삼각형 모양 피라미드를 만드시오
             for (int line = 0; line < size; line++)
             {
-               for (int blank = size - line; blank > 0; blank--)
+                for (int blank = size - line; blank > 0; blank--)
                     Console.Write(" ");
-               for (int star = 0; star <= line * 2; star++)
+                for (int star = 0; star <= line * 2; star++)
                     Console.Write("*");
-               Console.Write("\n");
+                Console.Write("\n");
             }
             Console.ReadLine();
             return;
@@ -244,7 +247,7 @@ namespace ConsoleApp1
             int[] array = new int[5];
             for (int i = 0; i < 5; i++)
             {
-                Console.Write((i+1)+"번째 정수를 입력하세요:");
+                Console.Write((i + 1) + "번째 정수를 입력하세요:");
                 array[i] = int.Parse(Console.ReadLine());
             }
             //가공-출력
@@ -253,8 +256,8 @@ namespace ConsoleApp1
                 Console.Write(item + " ,");
             Console.WriteLine("입니다.");
             //최대,최소 가공 처리 출력
-            Console.WriteLine("입력받은 가장 작은 수는 \""+array.Min()+"\"입니다.");
-            Console.WriteLine("입력받은 가장 큰 수는 \""+array.Max()+"\"입니다.");
+            Console.WriteLine("입력받은 가장 작은 수는 \"" + array.Min() + "\"입니다.");
+            Console.WriteLine("입력받은 가장 큰 수는 \"" + array.Max() + "\"입니다.");
             Console.ReadLine();
             return;
         }
@@ -269,20 +272,38 @@ namespace ConsoleApp1
             다섯 번째 수열: 1이 2개, 2가 1개, 1이 1개 = 122111
             여섯 번째 수열: 1이 1개, 2가 2개 1이 3개 = 112213
             */
-            int[] n = { 1 };
-            List<String> antSequence = new List<String>();
-            Console.WriteLine(n[0] + " " + n.Length);
-            antSequence.Add(n + " " + n.Length);
-            Console.WriteLine(antSequence.Count);
+            string input = "1";
 
-            antSequence.Add(n + " " + n.Length);
-            Console.WriteLine(antSequence.Count);
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine($"{i} : {input}");
+                //읽고 말하기 수열 계산
+                string result = "";
+                char number = input[0];
+                int count = 0; //number의 개수
+                for (int j = 0; j < input.Length; j++)
+                {
+                    //문자열과 가리키는 숫자가 같지 않다면 
+                    if (number != input[j])
+                    {
+                        result = result + number + count;
+                        number = input[j]; //입력값을 가리키는 숫자에 넣는다
+                        count = 1;
+                    }
+                    //같다면 카운터를 올린다.
+                    else
+                    {
+                        count++;
+                    }
+                }
+                result = result + number + count;
 
-            antSequence.Add(n + " " + n.Length);
-            Console.WriteLine(antSequence.Count);
+                input = result;
+            }
+
             Console.ReadLine();
             return;
         }
+    }//끝 class practice01
+}//끝 namespace ConsoleApp1
 
-    }
-}
